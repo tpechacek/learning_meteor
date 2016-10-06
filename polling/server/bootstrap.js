07488ebf243302ac5e78eeabe6a6927d3f35c986
tpechacek <taylortpechacek@gmail.com>
2016-10-06 15:22:53 -0400

    New polling app example

---
+++ polling/server/bootstrap.js
@@ -0,0 +1,34 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+  if (Polls.find().count() === 0) {
+
+  	// create sample polls
+  	var samplePolls = [
+  		{
+  			question: 'Is Meteor awesome?',
+  			choics: [
+  				{ text: 'Of course!', votes: 0 },
+  				{ text: 'Eh', votes: 0 },
+  				{ text: 'No. I like plain JS', votes: 0 }
+  			]
+  		},
+  		{
+  			question: 'is CSS3 Flexbox the greatest thing since array_slice(bread)?',
+  			choices: [
+  				{ text: '100% yes', votes: 0 },
+  				{ text: '200% yes', votes: 0 },
+  				{ text: '300% yes', votes: 0 }
+  			]
+  		}
+  	];
+
+  	// loop over each sample poll and insert into database
+  	_.each(samplePolls, function(poll) {
+  		Polls.insert(poll);
+  	});
+
+  }
+
+});
